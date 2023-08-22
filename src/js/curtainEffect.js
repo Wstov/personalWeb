560618ba5d99c935d8d23437323aac120135f745
Gustavo Marin <wstov@hotmail.com>
2023-08-21 23:32:10 -0600

    Section 3 styles

--- src/js/curtainEffect.js
+++ src/js/curtainEffect.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", function() {
     const curtain = document.querySelector(".curtain");
     const content = document.querySelector(".content");
-    const delay = 2000; // 2 segundos
+    const delay = 800; // 2 segundos
 
     setTimeout(function() {
         curtain.classList.add("hide");
